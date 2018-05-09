74b7fd2459874ed5a8e39ccf9a94391f2b91c50b
Sebastian <sebastian.lin@xinzedigital.com>
2018-05-09 15:47:18 +0800

    Add more ABI

--- android/simple_apk/Application.mk
+++ android/simple_apk/Application.mk
@@ -1,5 +1,7 @@
 APP_PLATFORM := android-8
-APP_ABI := armeabi-v7a
+#APP_ABI := armeabi-v7a
+#APP_ABI := arm64-v8a x86_64
+APP_ABI := arm64-v8a
 LOCAL_ARM_NEON=true
 ARCH_ARM_HAVE_NEON=true
 # TODO: Have libjpeg do this
