f2eba868c9a7faf056bd86a90c322bcfa337f00d
Sebastian <sebastian.lin@xinzedigital.com>
2018-06-27 14:13:05 +0800

    Add mutex lock

--- c/pthread/simple/main.c
+++ c/pthread/simple/main.c
@@ -4,11 +4,14 @@
 #include <pthread.h>
 #include <stdio.h>
 
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
 //The function we want to make the thread run.
 void task(void *msg)
 {
+	pthread_mutex_lock(&mtx);
 	printf("%s running.\n", (char*)msg);
-	while(1);
+	pthread_mutex_unlock(&mtx);
 }
 
 int main()
@@ -17,10 +20,17 @@ int main()
 	pthread_t id1, id2;
 	char *data1="Task1", *data2="Task2";
 
+	pthread_mutex_lock(&mtx);
+
 	// Constructs the new thread and runs it. Does not block execution.
 	ret=pthread_create(&id1, NULL, (void *) &task, (void *) data1);
 	ret=pthread_create(&id2, NULL, (void *) &task, (void *) data2);
 	
+	printf("main thread waiting...\n");
+	sleep(10);
+	pthread_mutex_unlock(&mtx);
+
+
 	//Makes the main thread wait for the new thread to finish execution, therefore blocks its own execution.
 	pthread_join(id1, NULL);
 	pthread_join(id2, NULL);
